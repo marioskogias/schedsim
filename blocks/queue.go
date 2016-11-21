c5e173797506820fae2d6aea93fe5f93fc0aa173
Marios Kogias <marios.kogias@epfl.ch>
2016-11-21 15:29:11 +0100

    Implement a priority queue

--- blocks/queue.go
+++ blocks/queue.go
@@ -1,9 +1,11 @@
 package blocks
 
 import (
+	"container/heap"
 	"container/list"
 )
 
+// Simple FIFO queue
 type Queue struct {
 	l *list.List
 }
@@ -26,4 +28,46 @@ func (q *Queue) Dequeue() interface{} {
 
 func (q *Queue) Len() int {
 	return q.l.Len()
+}
+
+// PriorityQueue
+type Comparable interface {
+	GetCmpVal() float64
+}
+
+type pQueue []Comparable
+
+func (pq pQueue) Len() int { return len(pq) }
+
+func (pq pQueue) Less(i, j int) bool {
+	return pq[i].GetCmpVal() < pq[j].GetCmpVal() // greater time - less priority
+}
+
+func (pq pQueue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+}
+
+func (pq *pQueue) Push(x interface{}) {
+	item := x.(Comparable)
+	*pq = append(*pq, item)
+}
+
+func (pq *pQueue) Pop() interface{} {
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	*pq = old[0 : n-1]
+	return item
+}
+
+type PQueue struct {
+	pq pQueue
+}
+
+func NewPQueue() *PQueue {
+	q := &PQueue{}
+	q.pq = make(pQueue, 0)
+	heap.Init(&q.pq)
+
+	return q
 }
